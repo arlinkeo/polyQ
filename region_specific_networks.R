68c49d3e3d817ec58de9bb620a3b49b5c624a74b
arlinkeo <arlinkeo@gmail.com>
2016-10-20 23:43:22 +0200

    rerun with two cerebellar structures

--- region_specific_networks.R
+++ region_specific_networks.R
@@ -1,54 +1,56 @@
 #Plotting region-specific networks
-setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression/regional_coexpression")
+setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression/regional_coexpression/")
 library(WGCNA)
 options(stringsAsFactors = FALSE)
 
-load("polyQ.RData")
-#id <- structureIDs[structureIDs$name == "pons", ]
-#load("pons/polyQgenes_Pons.RData")
+load("../resources/polyQ.RData")
+ontology <- read.csv("../ABA_human_processed/Ontology_edited.csv")
+id <- ontology[ontology$name %in% "cerebellar cortex", ][ , c(1:3)]
+id[3] <- "cerebellum"
+load(paste("cerebellum/subsetCor_", id[2], ".RData", sep = ""))
 
-# #Heatmap of modules averaged
-# modules <- rep(polyQgenes, each = 26)
-# #modules <- if (length(dupGenes) == 0 ) modules else modules[-dupGenes]
-# moduleMat <- subsetCor
-# rownames(moduleMat) <- modules
-# colnames(moduleMat) <- modules
-# diag(moduleMat) <- NA
-# 
-# #Function to calculate mean correlation of pairwise modules, returns Module matrix with mean values
-# meanPerMod <- function (x) {
-#   mods <- unique(colnames(x))
-#   mat <- matrix(NA, length(mods), length(mods))
-#   rownames(mat) <- mods
-#   colnames(mat) <- mods
-#   for (r in mods) {
-#     for (c in mods) {
-#       modPair <- subsetCor[which(rownames(x) == r), which(colnames(x) == c)]
-#       mat[r, c] <- mean(modPair, na.rm = TRUE)
-#     }
-#   }
-#   mat
-# }
-# 
-# moduleMeans <- meanPerMod(moduleMat)
-# save(moduleMeans, file = paste(gsub(" ", "_", id[3]), "/moduleMeans_", id[2], ".RData", sep = ""))
+#Heatmap of modules averaged
+modules <- rep(polyQgenes, each = 26)
+#modules <- if (length(dupGenes) == 0 ) modules else modules[-dupGenes]
+moduleMat <- subsetCor
+rownames(moduleMat) <- modules
+colnames(moduleMat) <- modules
+diag(moduleMat) <- NA
+
+#Function to calculate mean correlation of pairwise modules, returns Module matrix with mean values
+meanPerMod <- function (x) {
+  mods <- unique(colnames(x))
+  mat <- matrix(NA, length(mods), length(mods))
+  rownames(mat) <- mods
+  colnames(mat) <- mods
+  for (r in mods) {
+    for (c in mods) {
+      modPair <- subsetCor[which(rownames(x) == r), which(colnames(x) == c)]
+      mat[r, c] <- mean(modPair, na.rm = TRUE)
+    }
+  }
+  mat
+}
+
+moduleMeans <- meanPerMod(moduleMat)
+save(moduleMeans, file = paste(gsub(" ", "_", id[3]), "/moduleMeans_", id[2], ".RData", sep = ""))
 
 make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(x)))))}
 labels <- as.vector(rbind(matrix("", 12, 9), polyQgenes, matrix("", 13, 9)))
 
 for (i in 1:8) {
-  id <- structureIDs[i, ]
-  filename <- paste(gsub(" ", "_", id[3]), "/polyQgenes_", id[2], ".RData", sep = "")
-  print(filename)
-  attach(filename)
+  # id <- structureIDs[i, ]
+  # filename <- paste(gsub(" ", "_", id[3]), "/polyQgenes_", id[2], ".RData", sep = "")
+  # print(filename)
+  # attach(filename)
   
   pdf(file = paste(gsub(" ", "_", id[3]), "/polyQgenes_heatmap_", id[2], ".pdf", sep = ""), 8, 9)
   par(mar = c(5,6,12,3));
   labeledHeatmap(subsetCor, xLabels = make.italic(labels), colors = blueWhiteRed(200), zlim = c(-1,1), setStdMargins = FALSE, 
                  main = paste("polyQ top25 in ", id[3], sep =""), cex.lab = 1.3)
-  detach(2)
-  attach(paste(gsub(" ", "_", id[3]), "/moduleMeans_", id[2], ".RData", sep = ""))
-  
+  # detach(2)
+  # attach(paste(gsub(" ", "_", id[3]), "/moduleMeans_", id[2], ".RData", sep = ""))
+  # 
   labeledHeatmap(moduleMeans, xLabels = make.italic(colnames(moduleMeans)), colors = blueWhiteRed(200), zlim = c(-1,1), setStdMargins = FALSE, 
                  main = paste("Mean correlation within modules in ", id[3], sep =""), cex.lab = 1.3, textMatrix = round(moduleMeans, digits = 2))
   
@@ -56,7 +58,7 @@ for (i in 1:8) {
   geneTree = hclust(as.dist(1-moduleMeans), method = "average");
   order <- geneTree$order
   orderMods <- moduleMeans[order, order]
-  detach(2)
+  # detach(2)
   
   par(mar = c(33.5,6.5,4,6.5), fig=c(0,1,0,1))
   plot(geneTree, xlab="", sub="", main = paste("Mean correlation of clustered modules in ", id[3], sep =""), 

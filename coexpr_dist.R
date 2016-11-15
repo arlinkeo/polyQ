76c339170de7335b9d30255d183fff6b4186de97
arlinkeo <arlinkeo@gmail.com>
2016-11-15 13:03:09 +0100

    print quantiles info

--- coexpr_dist.R
+++ coexpr_dist.R
@@ -33,17 +33,25 @@ lapply(polyQgenes, function(pq){
   plot(0, type = "n", axes=F, xlab="", ylab="")
   mtext(make.italic(pq), 3, line = 0)
 })
-lapply(names(coexpr_dist), function(r){
+quantiles <- sapply(names(coexpr_dist), function(r){
   m <- coexpr_dist[[r]]
-  lapply(pQEntrezIDs, function(x){
+  res <- sapply(pQEntrezIDs, function(x){
     vector <- m[x, ]
     par(mai = c(0.15, 0.15, 0.15, 0.15))
     hist(vector, breaks = seq(-1,1,by=0.05), xlab = NULL, ylab = NULL, xlim = c(-1, 1), ylim = c(0, 4000), main = NULL, cex.axis = 0.8)
     q95 <- quantile(vector, probs = 0.95)
     abline(v = q95, col = "red")
+    q95
   })
   mtext(gsub("_", " ", r), 3, line = 65, cex = 1.2)
+  res
 })
 title("PolyQ co-expression distribution", outer = TRUE, cex.main = 3)
 title(xlab = "Co-expression", ylab = "Frequency", outer = TRUE, cex.lab = 2.5)
 dev.off()
+
+#Print quantile info
+quantiles
+mean(quantiles)
+median(quantiles)
+sd(quantiles)

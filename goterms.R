b7f00200cf5a66a977dbbd1b9214756fa896d4ba
arlinkeo <arlinkeo@gmail.com>
2016-11-16 13:07:44 +0100

    add background, changed threshold

--- goterms.R
+++ goterms.R
@@ -11,10 +11,19 @@ structureIDs <- structureIDs[!structureIDs$name %in% c("cerebellar nuclei","basa
 structureIDs[, 3] <- sapply(structureIDs[, 3], function(x){gsub(" ", "_", x)})
 probeInfo <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")
 entrezId2Name <- function (x) { row <- which(probeInfo$entrez_id == x); probeInfo[row, 4]} #Input is single element
-load("resources/genesets_threshold070.RData")
+load("resources/genesets_threshold050.RData")
 region.acronym <- function(x) {structureIDs[structureIDs$name %in% x, ]$acronym}
+
 # Open connection to DAVID
-david<-DAVIDWebService$new(email="D.L.Keo@tudelft.nl", url="https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
+david<-DAVIDWebService$new(email="D.L.Keo@tudelft.nl", 
+                           url="https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
+setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL", "KEGG_PATHWAY", "REACTOME_PATHWAY", "BIOCARTA", 
+                                 "BIOGRID_INTERACTION", "DIP", "INTACT", "MINT", "UCSC_TFBS"))
+
+# Background list from AHBA probe info
+bg_list <- probeInfo$entrez_id
+bg <- addList(david, bg_list, idType = "ENTREZ_GENE_ID", listName = "AHBA background", listType = "Background")
+bg
 
 # Obtain a list of GO terms for each gene set
 t <- 0.05 # EASE p-value threshold
@@ -23,10 +32,11 @@ lapply(names(regionLs), function(r){
     if (length(pq) > 1){
       pqname <- entrezId2Name(pq[1])
       listname <- paste(r, "_", pqname, sep = "")
-      # Backgroundlist?
       result <- addList(david, pq, idType = "ENTREZ_GENE_ID", listName = listname, listType = "Gene")
-      fname <- paste("regional_coexpression/", r, "/goterms070_", region.acronym(r), "_", pqname, ".txt", sep = "")
+      result
+      fname <- paste("regional_coexpression/", r, "/goterms050_", region.acronym(r), "_", pqname, ".txt", sep = "")
       getFunctionalAnnotationChartFile(david, fname, threshold=t, count=2L)
     }
   })
 })
+david

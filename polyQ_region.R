6c038f6f5b084b811a3a7a450e4aefb7cd35904b
arlinkeo <arlinkeo@gmail.com>
2017-01-27 00:39:58 +0100

    Dir location updated

--- polyQ_region.R
+++ polyQ_region.R
@@ -1,9 +1,9 @@
-setwd("C:/Users/dkeo/Documents/Human-brain-project/polyQgenes")
+setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression")
 library(WGCNA)
 options(stringsAsFactors = FALSE)
 
 #Probe info (entrez_id)
-entrez_id <- read.csv("../ABA_human_processed/probe_info_2014-11-11.csv")[ , 6]
+entrez_id <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")[ , 6]
 
 #Read expression data, assign row- and columnnames
 brainNames <- list("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
@@ -16,13 +16,13 @@ brainExpr <- lapply(brainNames, function (x) {
 save(brainExpr, file = "BrainExpr.RData")
 
 # Sample structures
-ontology <- read.csv("../ABA_human_processed/Ontology_edited.csv")
-structures <- c("frontal lobe", "globus pallidus", "mesencephalon", "basal forebrain", "striatum", "hypothalamus", "pons", "cerebellum")
+ontology <- read.csv("ABA_human_processed/Ontology_edited.csv")
+structures <- c("brain","frontal lobe", "mesencephalon", "striatum", "hypothalamus", "pons", "parietal lobe", "cerebellar cortex")
 structureIDs <- ontology[ontology$name %in% structures, ][ , c(1:3)]
 
 #Select PolyQ genes from data
 polyQgenes <- c("HTT", "ATN1", "AR", "ATXN1", "ATXN2", "ATXN3", "CACNA1A", "ATXN7", "TBP")
-probeInfo <- read.csv("../ABA_human_processed/probe_info_2014-11-11.csv")
+probeInfo <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")
 pQgeneInfo <- probeInfo[probeInfo$gene_symbol %in% polyQgenes, ]
 pQEntrezIDs <- as.character(pQgeneInfo[ , 6])
 polyQgenes <- pQgeneInfo[ , 4]

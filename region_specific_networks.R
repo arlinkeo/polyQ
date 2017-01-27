2fb0bda844e924e6a26ac6f97bb7daeb9260a171
arlinkeo <arlinkeo@gmail.com>
2017-01-27 01:07:49 +0100

    ..

--- region_specific_networks.R
+++ region_specific_networks.R
@@ -5,12 +5,11 @@ options(stringsAsFactors = FALSE)
 
 load("../resources/polyQ.RData")
 ontology <- read.csv("../ABA_human_processed/Ontology_edited.csv")
-id <- ontology[ontology$name %in% "cerebellar nuclei", ][ , c(1:3)]
+id <- ontology[ontology$name %in% "parietal lobe", ][ , c(1:3)]
 load(paste(gsub(" ", "_", id[3]), "/subsetCor_", id[2], ".RData", sep = ""))
 
 #Heatmap of modules averaged
 modules <- rep(polyQgenes, each = 26)
-#modules <- if (length(dupGenes) == 0 ) modules else modules[-dupGenes]
 moduleMat <- subsetCor
 rownames(moduleMat) <- modules
 colnames(moduleMat) <- modules
@@ -38,18 +37,11 @@ make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(
 labels <- as.vector(rbind(matrix("", 12, 9), polyQgenes, matrix("", 13, 9)))
 
 for (i in 1:8) {
-  # id <- structureIDs[i, ]
-  # filename <- paste(gsub(" ", "_", id[3]), "/polyQgenes_", id[2], ".RData", sep = "")
-  # print(filename)
-  # attach(filename)
-  
   pdf(file = paste(gsub(" ", "_", id[3]), "/polyQgenes_heatmap_", id[2], ".pdf", sep = ""), 8, 9)
   par(mar = c(5,6,12,3));
   labeledHeatmap(subsetCor, xLabels = make.italic(labels), colors = blueWhiteRed(200), zlim = c(-1,1), setStdMargins = FALSE, 
                  main = paste("polyQ top25 in ", id[3], sep =""), cex.lab = 1.3)
-  # detach(2)
-  # attach(paste(gsub(" ", "_", id[3]), "/moduleMeans_", id[2], ".RData", sep = ""))
-  # 
+
   labeledHeatmap(moduleMeans, xLabels = make.italic(colnames(moduleMeans)), colors = blueWhiteRed(200), zlim = c(-1,1), setStdMargins = FALSE, 
                  main = paste("Mean correlation within modules in ", id[3], sep =""), cex.lab = 1.3, textMatrix = round(moduleMeans, digits = 2))
   
@@ -57,8 +49,7 @@ for (i in 1:8) {
   geneTree = hclust(as.dist(1-moduleMeans), method = "average");
   order <- geneTree$order
   orderMods <- moduleMeans[order, order]
-  # detach(2)
-  
+
   par(mar = c(33.5,6.5,4,6.5), fig=c(0,1,0,1))
   plot(geneTree, xlab="", sub="", main = paste("Mean correlation of clustered modules in ", id[3], sep =""), 
        hang = 0.04, font = 3, axes = FALSE, ylab = "");
@@ -66,5 +57,4 @@ for (i in 1:8) {
   labeledHeatmap(orderMods, xLabels = make.italic(colnames(orderMods)), colors = blueWhiteRed(200), zlim = c(-1,1), 
                  setStdMargins = FALSE, textMatrix = round(orderMods, digits = 2))
   dev.off()
-  
 }

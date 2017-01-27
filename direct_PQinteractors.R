e78018ace8215e514d32901c2e56a673fbacdfcb
arlinkeo <arlinkeo@gmail.com>
2017-01-27 21:03:18 +0100

    save and plot

--- direct_PQinteractors.R
+++ direct_PQinteractors.R
@@ -24,7 +24,7 @@ rownames(genepairs) <- apply(genepairs, 1, function(x) {paste(x[1], "-", x[2], s
 reduce2pq <- function(r){lapply(r, function(x){intersect(x, pQEntrezIDs)})}
 pqInteractors <- lapply(regionLs, reduce2pq)
 pqInteractors <- lapply(pqInteractors, function(r){r[which(lengths(r) != 1)]}) # remove vectors of size 1
-pqInteractors <- pqInteractors[which(lengths(pqInteractors) != 0)] # remove empty lists (regions)
+# pqInteractors <- pqInteractors[which(lengths(pqInteractors) != 0)] # remove empty lists (regions)
 pqInteractors <- lapply(pqInteractors, function(x){names <- lapply(names(x), entrezId2Name); names(x) <- names; x})
 pqInteractors <- lapply(pqInteractors, function(x){lapply(x, function(y){sapply(y, entrezId2Name)})})
 
@@ -43,3 +43,49 @@ matrixList <- lapply(pqInteractors, function(r){
   })
 })
 
+#Save pdf of matrices
+pdf(file = "polyQ_neighbors_matrices.pdf", 8, 9)
+par(mar = c(5,6,12,3));
+lapply(names(matrixList), function(r){
+  mat <- matrixList[[r]]
+  labeledHeatmap(mat, xLabels = make.italic(colnames(mat)), colors = c("white", "red"), plotLegend = FALSE,
+                 setStdMargins = FALSE, main = paste("PolyQ neighbours in ", gsub("_", " ", r), sep =""), 
+                 cex.lab = 1.3, textMatrix = mat)
+})
+dev.off()
+
+####Save pdf of table with all polyQ pairs
+
+table <- sapply(matrixList, function(mat){
+  apply(genepairs, 1, function(c){
+    mat[c[1], c[2]]
+  })
+})
+
+#Load asssociations info from literature
+associations <- read.csv("datatype_interactions.txt", sep = "\t", row.names = 1, comment.char = "#")
+associations <- associations[ , c(15:17, 1:14)]
+# Sort rows by associations
+SCA_and_HD <- which(bitwAnd(associations$SCA_total, associations$HD_total) == 1)
+SCA <- which(associations$SCA_total == 1)
+only_SCA <- SCA[-which(SCA == SCA_and_HD)]
+HD <- which(associations$HD_total == 1)
+only_HD <- HD[-which(HD == SCA_and_HD)]
+only_HD <- only_HD[order(associations[only_HD, ]$SCA_total)]
+not_SCA_and_HD <- c(which(associations$SCA_or_HD == 0), which(is.na(associations$SCA_or_HD)))
+order <- c(only_SCA, SCA_and_HD, only_HD, not_SCA_and_HD)
+associations <- associations[order, ]
+
+table <- cbind(table, associations)
+
+# prepare gene pair matrix from rownames(associations) with entrezIds
+genepairs <- t(sapply(rownames(associations), function(x){as.character(unlist(strsplit(x, "-")))}))
+
+pdf(file = "polyQ_neighbors_table.pdf", 21, 14)
+par(mar = c(6, 10, 15, 4))
+par(mai = c(0.5, 2, 3, 0.5))
+  labeledHeatmap(table, xLabels = colnames(table), yLabels = make.italic(rownames(table)),
+               setStdMargins = FALSE, xLabelsPosition = "top", xLabelsAdj = 0, colors = c("white", "red"), plotLegend = FALSE,
+               textMatrix = table, main = "PolyQ neighbours co-expressed >0.5")
+
+dev.off()

0cf1e9e154cd00189cfb2362875a40533555155e
arlinkeo <arlinkeo@gmail.com>
2017-02-06 08:26:37 +0100

    export to cytoscape with package RCy3

--- overlap_samples.R
+++ overlap_samples.R
@@ -66,18 +66,53 @@ avgExpr <- sapply(sampleIDs, function(s){
   apply(res, 1, mean) # Avg across region-specific samples and donors per PQ
 })
 rownames(avgExpr) <- sapply(rownames(avgExpr), entrezId2Name)
+pal <- colorRampPalette(c('darkgreen', 'yellow', 'red'))
+avgExprColor <- matrix(pal(100)[as.numeric(cut(avgExpr, breaks = 100))], 9, 9)
+rownames(avgExprColor) <- rownames(avgExpr)
+colnames(avgExprColor) <- colnames(avgExpr)
 
 load("resources/pqNeighbors.RData")
 
 # save node and edge file for each structure
+pqPairs <- t(combn(polyQgenes, 2))
 apply(structureIDs, 1, function(id){
   id <- unlist(id)
   structName <- id[2]
   structure <- id[3]
-  cyt <- exportNetworkToCytoscape(pqNeighbors[[structure]], threshold = -1, nodeAttr = avgExpr[, structure], 
-                                  edgeFile = paste("regional_coexpression/", structure, "/pqNeighborsEdges_",structName,".txt", sep = ""), 
-                                nodeFile = paste("regional_coexpression/", structure, "/pqExprNodes_", structName,".txt", sep = ""))
+  mat <- pqNeighbors[[structure]]
+  
+  nodeTable  <- data.frame(nodeName = polyQgenes, expr = avgExpr[, structure], exprColor = avgExprColor[, structure])
+  edgeTable <- data.frame(fromNode = pqPairs[ , 1], toNode = pqPairs[ , 2], edgeType="interaction",
+                          interaction = apply(pqPairs, 1, function(x){mat[x[1], x[2]]}))
+  g <- cyPlot(nodeTable, edgeTable)
+  windowName <- paste("pqNeighbors_",structName, sep = "")
+  cw <- CytoscapeWindow(windowName, graph = g, overwrite = TRUE)
+  displayGraph (cw)
+  layoutNetwork (cw, layout.name = "circular")
+  setDefaultNodeShape (cw, "Ellipse")
+  setDefaultNodeFontSize (cw, 8)
+  setNodeColorDirect(cw,  nodeTable[ , "nodeName"],  nodeTable[ , "exprColor"])
+  #lockNodeDimensions (cw, TRUE)
+  setNodeHeightDirect(cw, nodeTable[ , "nodeName"], 30)
+  setNodeWidthDirect(cw, nodeTable[ , "nodeName"], 30)
+  #showGraphicsDetails(cw, TRUE)
+  rowCol <- which(mat == 0, arr.ind = TRUE)
+  nonEdges <- apply(rowCol, 1, function(x){
+    row <- x[1]
+    col <- x[2]
+    paste(rownames(mat)[row], colnames(mat)[col], sep = " (interaction) ")
+  })
+  selectEdges(cw, nonEdges)
+  deleteSelectedEdges(cw)
+  svgName <- paste("regional_coexpression/", structure, "/", windowName, ".svg", sep = "")
+  saveImage(cw, svgName, "svg")
+  # cyt <- exportNetworkToCytoscape(pqNeighbors[[structure]], threshold = -1, nodeAttr = attr, 
+  #                                 edgeFile = paste("regional_coexpression/", structure, "/pqNeighborsEdges_",structName,".txt", sep = ""), 
+  #                               nodeFile = paste("regional_coexpression/", structure, "/pqExprNodes_", structName,".txt", sep = ""))
 })
-cyt <- exportNetworkToCytoscape(pqNeighbors[["HD_region"]], threshold = -1, nodeAttr = avgExpr[, "HD_region"], 
+
+attr <- cbind(avgExpr[, "HD_region"], avgExprColor[, "HD_region"])
+colnames(attr) <- c("expr", "exprColor")
+cyt <- exportNetworkToCytoscape(pqNeighbors[["HD_region"]], threshold = -1, nodeAttr = attr, 
                                 edgeFile = "HD_masks_Coppen2016/pqNeighborsEdges_HDregion.txt", 
                                 nodeFile = "HD_masks_Coppen2016/pqExprNodes_HDregion.txt")

2e58fb84da31929de81741018121a36fb37e49b1
arlinkeo <arlinkeo@gmail.com>
2017-02-10 16:03:03 +0100

    overlap function in separate script save objects

--- overlap_goterms.R
+++ overlap_goterms.R
@@ -6,12 +6,17 @@ options(stringsAsFactors = FALSE)
 
 #Prepare data and functions
 load("resources/polyQ.RData")
+structureIDs[, 3] <- sapply(structureIDs[, 3], function(id){gsub(" ", "_", id)})
+structureIDs <- rbind(structureIDs, c(NA, "HDregion", "HD_region"))
+rownames(structureIDs) <- structureIDs$name
 structures <- split(structureIDs, seq(nrow(structureIDs)))
 names(structures) <- structureIDs$name
 probeInfo <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")
 entrezId2Name <- function (x) { row <- which(probeInfo$entrez_id == x); probeInfo[row, 4]} #Input is single element
 name2entrezId <- function (x) { row <- which(probeInfo$gene_symbol == x); probeInfo[row, 6]} #Input is single element
 make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(x)))))}
+setOverlap <- dget("polyQ_scripts/setOverlap.R")
+setOverlapSignif <- dget("polyQ_scripts/setOverlapSignif.R")
 
 #Load asssociations info from literature
 associations <- read.csv("datatype_interactions.txt", sep = "\t", row.names = 1, comment.char = "#")
@@ -26,19 +31,6 @@ only_HD <- only_HD[order(associations[only_HD, ]$SCA_total)]
 not_SCA_and_HD <- c(which(associations$SCA_or_HD == 0), which(is.na(associations$SCA_or_HD)))
 order <- c(only_SCA, SCA_and_HD, only_HD, not_SCA_and_HD)
 associations <- associations[order, ]
-
-# prepare gene pair matrix from rownames(associations) with entrezIds
-genepairs <- t(sapply(rownames(associations), function(x){as.character(sapply(unlist(strsplit(x, "-")), name2entrezId))}))
-
-# Function to count number of overlapping GO terms between two polyQ sets per region
-overlap <- function(x) {
-  apply(genepairs, 1, function(y){
-    termset1 <- x[[y[1]]]
-    termset2 <- x[[y[2]]]
-    terms <- intersect(termset1, termset2)
-    length(terms)
-  })
-}
 
 #Function to read Rdavid output
 read.RdavidOutput <- function(fileName){
@@ -61,24 +53,29 @@ names(pQEntrezIDs) <- pQEntrezIDs
 ll <- lapply(structures, function(r){
   lapply(pQEntrezIDs, function(pq){
     pqName <- entrezId2Name(pq)
-    fName <- paste("regional_coexpression/", gsub(" ", "_", r[3]), "/goterms050_", r[2], "_", pqName, ".txt", sep = "")
+    fName <- paste("regional_coexpression/", r[3], "/goterms050_", r[2], "_", pqName, ".txt", sep = "")
     print(fName)
     read.RdavidOutput(fName)
   })
 })
-ll_HDregion <- lapply(pQEntrezIDs, function(pq){
-  pqName <- entrezId2Name(pq)
-  fName <- paste("HD_masks_Coppen2016/goterms050_HDregions", "_", pqName, ".txt", sep = "")
-  print(fName)
-  read.RdavidOutput(fName)
-})
-ll <- c(ll, HD_region = list(ll_HDregion))
-rm(ll_HDregion)
 # Lists without multiple testing
 ll1 <- lapply(ll, function(r){lapply(r, function(pq){pq$Term})})
 # Filter terms based on Benjamini p-value < 0.05
 ll2 <- lapply(ll, function(r){lapply(r, function(pq){pq[pq$Benjamini < 0.05, ]$Term})})
 rm(ll)
+
+ll2 <- lapply(ll2, function(s){
+  names(s) <- sapply(names(s), entrezId2Name)# pQ genes to entrezID
+  s
+})
+termSetOverlap <- lapply(ll2, setOverlap)
+termSetOverlapSignif <- lapply(ll2, setOverlapSignif)
+save(termSetOverlap, file = "resources/termSetOverlap.RData")
+save(termSetOverlapSignif, file = "resources/termSetOverlapSignif.RData")
+
+
+
+
 
 # Plot table with number of terms in each geneset
 plot.numbers <- function(l, main = ""){
@@ -89,23 +86,6 @@ plot.numbers <- function(l, main = ""){
   labeledHeatmap(replace(table, which(table == 0), NA), xLabels = gsub("_", " ", colnames(table)), xLabelsPosition = "top", 
                  yLabels = c(make.italic(rownames(table)[-10]), rownames(table)[10]), colors = blueWhiteRed(200)[100:200], 
                  main = main, setStdMargins = FALSE, xLabelsAdj = 0, textMatrix = table)
-}
-
-# Function to get significance of overlap using hypergeometric test.
-hyper.test <- function(x) {
-  apply(genepairs, 1, function(y){
-    termset1 <- x[[y[1]]]
-    termset2 <- x[[y[2]]]
-    overlap <- length(intersect(termset1, termset2))
-    ngs1 <- length(termset1)
-    ngs2 <- length(termset2)
-    totalTermes <- 19992
-    if (overlap != 0){
-      print(paste(cat(sapply(y, entrezId2Name), sep = "-"), 
-                  ": phyper(", overlap, " - 1, ", ngs1, ", 19992 - ", ngs1, ", ", ngs2, ", lower.tail = FALSE)", sep = ""))
-    }
-    phyper(overlap - 1, ngs1, totalTermes - ngs1, ngs2, lower.tail = FALSE)
-  })
 }
 
 pdf(file = "number_of_goterms050.pdf", 8, 9)

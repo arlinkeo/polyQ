0dee6bd20ed5f0e4d7b7fd529cc58b80eabee036
arlinkeo <arlinkeo@gmail.com>
2017-03-01 17:51:15 +0100

    Table instead of matrix output

--- setOverlap.R
+++ setOverlap.R
@@ -1,18 +1,4 @@
 # Overlap between sets of genes/functional terms
-# setOverlap <- function(x) {
-#   setNames <- names(x)
-#   n <- length(setNames)
-#   mat <- matrix(0, n, n, dimnames = list(setNames, setNames))
-#   for (pq1 in setNames){
-#     for (pq2 in setNames){
-#       set1 <- x[[pq1]]
-#       set2 <- x[[pq2]]
-#       overlap <- intersect(set1, set2)
-#       mat[pq1, pq2] <- length(overlap)
-#     }
-#   }
-#   mat
-# }
 setOverlap <- function(x) {
   setNames <- names(x)
   n <- length(setNames)
--- setOverlapSignif.R
+++ setOverlapSignif.R
@@ -1,26 +1,4 @@
 # Significance of overlap between sets of genes/functional terms
-# setOverlapSignif <- function(x, total = 19992) {
-#   setNames <- names(x)
-#   n <- length(setNames)
-#   mat <- matrix(0, n, n, dimnames = list(setNames, setNames))
-#   for (pq1 in setNames){
-#     for (pq2 in setNames){
-#       set1 <- x[[pq1]]
-#       set2 <- x[[pq2]]
-#       overlap <- length(intersect(set1, set2))
-#       ns1 <- length(set1)
-#       ns2 <- length(set2)
-#       if (overlap != 0){
-#         print(paste(cat(pq1, pq2, sep = "-"), 
-#                     ": phyper(", overlap, " - 1, ", ns1, ", ", total, " - ", ns1, ", ", ns2, ", lower.tail = FALSE)", sep = ""))
-#       }
-#       pVal <- phyper(overlap - 1, ns1, total - ns1, ns2, lower.tail = FALSE)
-#       mat[pq1, pq2] <- pVal
-#     }
-#   }
-#   diag(mat) <- 1
-#   mat
-# }
 setOverlapSignif <- function(x, total) {
   setNames <- names(x)
   n <- length(setNames)

42f5e5d5af4713fea394c9544e380be65ca4b268
arlinkeo <arlinkeo@gmail.com>
2017-03-09 13:41:59 +0100

    dir

--- polyQ_region.R
+++ polyQ_region.R
@@ -7,9 +7,9 @@ entrez_id <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")[ , 6]
 #Read expression data, assign row- and columnnames
 brainNames <- list("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
 brainExpr <- lapply(brainNames, function (x) {
-  expr <- read.csv(paste("../ABA_human_processed/gene_expr_normalized_microarray_", x, "_2014-11-11.csv", sep = ""), header = FALSE)
+  expr <- read.csv(paste("ABA_human_processed/gene_expr_normalized_microarray_", x, "_2014-11-11.csv", sep = ""), header = FALSE)
   rownames(expr) <- entrez_id
-  colnames(expr) <- read.csv(paste("../ABA_human_processed/sample_info_normalized_microarray_", x, "_2014-11-11.csv", sep = ""))[ , 1]
+  colnames(expr) <- read.csv(paste("ABA_human_processed/sample_info_normalized_microarray_", x, "_2014-11-11.csv", sep = ""))[ , 1]
   expr
 })
 save(brainExpr, file = "resources/BrainExpr.RData")
@@ -18,6 +18,8 @@ save(brainExpr, file = "resources/BrainExpr.RData")
 ontology <- read.csv("ABA_human_processed/Ontology_edited.csv")
 structures <- c("brain","frontal lobe", "mesencephalon", "striatum", "hypothalamus", "pons", "parietal lobe", "cerebellar cortex", "cerebellum")
 structureIDs <- ontology[ontology$name %in% structures, ][ , c(1:3)]
+structureIDs[, 3] <- sapply(structureIDs[, 3], function(id){gsub(" ", "_", id)})
+rownames(structureIDs) <- structureIDs$name
 
 #Select PolyQ genes from data
 polyQgenes <- c("HTT", "ATN1", "AR", "ATXN1", "ATXN2", "ATXN3", "CACNA1A", "ATXN7", "TBP")

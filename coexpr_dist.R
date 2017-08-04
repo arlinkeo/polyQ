858d191568f39ddc9da6cd79385f3852f6b83437
arlinkeo <arlinkeo@gmail.com>
2017-08-04 15:45:05 +0200

    italic gene names, reduce space  by removing axis labels of multiple plots

--- coexpr_dist.R
+++ coexpr_dist.R
@@ -10,7 +10,7 @@ structures <- split(structureIDs, seq(nrow(structureIDs)))
 names(structures) <- structureIDs$name
 probeInfo <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")
 entrezId2Name <- function (x) { row <- which(probeInfo$entrez_id == x); probeInfo[row, 4]} #Input is single element
-# make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(x)))))}
+make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(x)))))}
 
 # Load data
 coexpr_dist <- lapply(structures, function(r){
@@ -30,15 +30,30 @@ par(oma = c(8,8,12,2), mai = c(0.2,0.2,0.5,0.2))
 layout(matrix(c(1:90), 9, 10), widths = c(2, rep(3, 9)), heights = rep(1, 9))
 lapply(polyQgenes, function(pq){
   plot(0, type = "n", axes=F, xlab="", ylab="")
-  mtext(pq, 3, line = 0)
+  mtext(make.italic(pq), 2, line = -3.5, at = .8, cex = 1.2)
 })
 quantiles <- sapply(names(coexpr_dist), function(r){
   m <- coexpr_dist[[r]]
   res <- sapply(pQEntrezIDs, function(x){
     vector <- m[x, ]
     vector <- vector[names(vector) !=  x] #remove corr. with itself
-    par(mai = c(0.15, 0.15, 0.15, 0.15))
-    hist(vector, breaks = seq(-1,1,by=0.05), xlab = NULL, ylab = NULL, xlim = c(-1, 1), ylim = c(0, 6000), main = NULL, cex.axis = 0.8)
+    par(mai = c(0.05, 0.05, 0.05, 0.05))#c(0.15, 0.15, 0.15, 0.15))
+    if (x == tail(pQEntrezIDs, 1)& r == head(names(coexpr_dist), 1)){#last row & first col
+      hist(vector, breaks = seq(-1,1,by=0.05), xlim = c(-1, 1), ylim = c(0, 6000), main = NULL, cex.axis = 1)
+    }else if (x == tail(pQEntrezIDs, 1)){#last row
+      hist(vector, breaks = seq(-1,1,by=0.05), yaxt='n', xlim = c(-1, 1), ylim = c(0, 6000), main = NULL, cex.axis = 1)
+      axis(side = 2, at = seq(0,6000,1000), labels = FALSE, tick = TRUE)
+    }
+    else if (r == head(names(coexpr_dist), 1)){#first col
+      hist(vector, breaks = seq(-1,1,by=0.05), xaxt='n', xlim = c(-1, 1), ylim = c(0, 6000), main = NULL, cex.axis = 1)
+      axis(side = 1, at = seq(-1,1,0.5), labels = FALSE, tick = TRUE)
+    }
+    else {
+      hist(vector, breaks = seq(-1,1,by=0.05), xaxt='n', yaxt='n', xlim = c(-1, 1), ylim = c(0, 6000), main = NULL, cex.axis = 1)
+      axis(side = 1, at = seq(-1,1,0.5), labels = FALSE, tick = TRUE)
+      axis(side = 2, at = seq(0,6000,1000), labels = FALSE, tick = TRUE)
+    }
+    
     q95 <- quantile(vector, probs = 0.95)
     abline(v = q95, col = "red")
     q95
@@ -47,7 +62,8 @@ quantiles <- sapply(names(coexpr_dist), function(r){
   res
 })
 title("PolyQ co-expression distribution", outer = TRUE, cex.main = 3)
-title(xlab = "Co-expression", ylab = "Frequency", outer = TRUE, cex.lab = 2.5)
+title(xlab = expression("Pearson\'s correlation ("~italic(r)~")"), outer = TRUE, cex.lab = 2, line = 3.5)
+title(ylab = "Frequency", outer = TRUE, cex.lab = 2, line = -2)
 dev.off()
 
 #Print quantile info

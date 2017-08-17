ca07d8744293c61039edcb131c00062cbfb3742d
arlinkeo <arlinkeo@gmail.com>
2017-08-17 14:45:12 +0200

    show signif. pval

--- diffExpr_HDregion.R
+++ diffExpr_HDregion.R
@@ -1,48 +1,24 @@
 #Differential expression in HD-associated region (Coppen2016)
 setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression")
 options(stringsAsFactors = FALSE)
-# library("reshape2")
-# library("RDAVIDWebService")
+library("reshape2")
+library("RDAVIDWebService")
 # library("metap")
 
 load("resources/polyQ.RData")
-probeInfo <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")
-entrezId2Name <- function (x) { row <- which(probeInfo$entrez_id == x); probeInfo[row, 4]} #Input is single element
-# make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(x)))))}
-load("resources/brainExpr.RData") 
+probeInfo <- read.csv("../ABA_human_processed/probe_info_2014-11-11.csv")
+entrezId2Name <- function (x) {probeInfo$gene_symbol[match(x, probeInfo$entrez_id)]} #Input is vector
+make.italic <- function(x) {as.expression(lapply(x, function(x) bquote(italic(.(x)))))}
+load("../ABA_Rdata/BrainExpr.RData")
 donorNames <- names(brainExpr)
 names(donorNames) <- donorNames
 load("resources/sampleIDs.RData")
 sampleIDs <- sampleIDs[!names(sampleIDs) %in% c("brain", "cerebellum")]
-# load("resources/polyQ.RData")
-# structureIDs <- structureIDs[!structureIDs$name %in% c("brain", "cerebellum"), ]
-# structureIDs <- rbind(HD_region = c(NA, "HDregion", "HD_region"), structureIDs)
-
 inHDregion <- sampleIDs[["HD_region"]] # Binary vector for each donor
-# inHDregion <- lapply(inHDregion, as.logical)
-
-#Concatenate binary info of all donors
-# inHDregion_all <- do.call(c, sampleIDs[["HD_region"]]) 
-# selection_all <- as.logical(inHDregion_all)
-
-#Concatenate expression data of all donors
-# expr_all <- do.call(cbind, brainExpr) # 19992*3702 matrix
 
 diffgenesPerBrain <- lapply(donorNames, function(b){
-  expr <- brainExpr[[b]]#[1:10, ]
+  expr <- brainExpr[[b]]#
   inHD <- as.logical(inHDregion[[b]])
-  
-  # exprIn <- expr[, inHD]
-  # exprOut <- expr[, !inHD]
-  # genes <- rownames(expr)
-  # genes <- t(sapply(genes, function(g){
-  #   gIn <- unlist(exprIn[g, ])
-  #   gOut <- unlist(exprOut[g, ])
-  #   diffUp <- wilcox.test(gIn, gOut, alternative = "greater")
-  #   diffDown <- wilcox.test(gIn, gOut, alternative = "less")
-  #   c(MedianExprIn = median(gIn), MedianExprOut = median(gOut), 
-  #     pval_up = diffUp$p.value, pval_down = diffDown$p.value)
-  # }))
 
   genes <- t(apply(expr, 1, function(g){
     g <- unlist(g)
@@ -51,6 +27,7 @@ diffgenesPerBrain <- lapply(donorNames, function(b){
     diffUp <- wilcox.test(exprIn, exprOut, alternative = "greater")
     diffDown <- wilcox.test(exprIn, exprOut, alternative = "less")
     c(MedianExprIn = median(exprIn), MedianExprOut = median(exprOut),
+      MeanExprIn = mean(exprIn), MeanExprOut = mean(exprOut),
       pval_up = diffUp$p.value, pval_down = diffDown$p.value)
   }))
   genes <- as.data.frame(genes)
@@ -58,8 +35,44 @@ diffgenesPerBrain <- lapply(donorNames, function(b){
   # genes$pval_down_cor <- p.adjust(genes$pval_down, method = "bonferroni", nrow(genes))
   genes
 })
+save(diffgenesPerBrain, file = "resources/diffgenesPerBrain.RData")
+load("resources/diffgenesPerBrain.RData")
 
-#Select genes p-values in all brains
+########################################################
+#Boxplots of pQ genes
+total <- length(pQEntrezIDs)
+
+pdf(file = "diffExpr_HDregion.pdf", 12, 16)
+layout(matrix(c(1:6), 6, 1), heights = rep(1, 6))
+par(oma = c(12,6,12,2), mai = c(0,0.6,0,0))
+lapply(donorNames, function(b){
+  expr <- brainExpr[[b]][pQEntrezIDs, ]
+  print(paste("max: ", max(expr), ", min: ", min(expr), sep = ""))
+  rownames(expr) <- sapply(rownames(expr), entrezId2Name)
+  inHD <- inHDregion[[b]]
+  expr <- as.data.frame(cbind(inHD, t(expr)))
+  v <- melt(expr, id.vars = "inHD")
+  ylab = paste("Donor", unlist(strsplit(b, split = "donor"))[2])
+  a <- boxplot(value~inHD+variable, v, col = c("turquoise", "orange"), xaxt = 'n', ylab = ylab, cex.lab = 2,
+               at = c(1:(total*3))[-seq(3, total*3, 3)], ylim= c(0,11), pch = '*')
+  pTab <- diffgenesPerBrain[[b]]
+  maxy <- max(a$out) +0.5
+  lapply(c(1:9), function(i){
+    pq <- pQEntrezIDs[i]
+    info <- pTab[pq,]
+    p <- if (info$pval_up < 0.025) info$pval_up else if (info$pval_down < 0.05) info$pval_down else ""
+    p <- format(p, digits = 2, scientific = TRUE)
+    text((i*3)-1.5 ,maxy, p)
+  })
+  if (b == donorNames[1]) legend("topright", c("Outside","Inside"), fill = c("turquoise", "orange"), cex = 2)
+})
+axis(1, at = seq(1.5, total*3,3), labels  = make.italic(entrezId2Name(pQEntrezIDs)), cex.axis = 2)
+title("Differential expression of polyQ genes", outer = TRUE, cex.main = 2.5)
+title( ylab = "Log expression", xlab = "Gene", outer = TRUE, cex.lab = 2)
+dev.off()
+
+#########################################
+#Select genes p-values in all brains (genes x brain)
 diffUp <- sapply(diffgenesPerBrain, function(b){
   res <- b$pval_up
   names(res) <- rownames(b)
@@ -78,111 +91,63 @@ allDown <- apply(diffDown, 1, function(g) {sum(g < 0.025) >= 5})
 downGenes <- names(allDown)[allDown]
 #sapply(downGenes, entrezId2Name)
 
+#Diff. expressed polyQ genes
 upGenes[which(upGenes %in% pQEntrezIDs)]
 downGenes[which(downGenes %in% pQEntrezIDs)]
 
-# combine p-values
-# genes_all <- rownames(expr_all)
-# combP <- sapply(genes_all, function(g){
-#   pvalues <- sapply(donorNames, function(b){
-#     diffgenesPerBrain[[b]][g, "corrected_p"]
-#   })
-#   fisherP <- sumlog(pvalues)
-#   unlist(fisherP[1:3])
-# })
-# combP <- as.data.frame(t(combP))
-# combP <- combP[order(combP$p), ]
-
-# avgDiffExpr <- apply(simplify2array(diffgenesPerBrain), 1:2, mean)
-# avgDiffExpr <- as.data.frame(avgDiffExpr)
-# avgDiffExpr$combinedP <- combP
-# avgDiffExpr$pVal <- NULL
-
-# save(pValues, file = "resources/diffExpr_pval.RData")
-#load("resources/diffExpr_pval.RData")
-
-#Entrez_id's of interesting genes
-diffGenes <- rownames(combP)[(combP$p < 0.001)]
-higherExprIn <- t(sapply(diffGenes, function(g){# T/F across donors
-  exprIn <-sapply(donorNames, function(b){ 
-    diffgenesPerBrain[[b]][g, "MedianExprIn"]
+values <- lapply(pQEntrezIDs, function(pq){
+  sapply(diffgenesPerBrain, function(b){
+    b[pq,]
   })
-  exprOut <-sapply(donorNames, function(b){
-    diffgenesPerBrain[[b]][g, "MedianExprOut"]
-  })
-  exprIn > exprOut
-}))
-higherInAll <- apply(higherExprIn, 1, function(x){Reduce("&", as.list(x))})# Logical vector
-lowerInAll <- apply(higherExprIn, 1, function(x){Reduce("&", as.list(!x))})
-upGenes <- names(which(higherInAll))
-upGenes <- data.frame(gene_symbol = sapply(upGenes, entrezId2Name), entrez_id = upGenes, 
-                      pvalue = sapply(combP[upGenes, "p"], function(x){format(x, digits =2, scientific = T)}))
-# upGenes <- upGenes[order(upGenes$pvalue), ]
-# write.table(upGenes, file = "diffExpr_upregulated.txt", sep = "\t", quote = FALSE, row.names = FALSE)
-downGenes <- names(which(lowerInAll))
-downGenes <- data.frame(gene_symbol = sapply(downGenes, entrezId2Name), entrez_id = downGenes, 
-                        pvalue = sapply(combP[downGenes, "p"], function(x){format(x, digits =2, scientific = T)}))
-# downGenes <- downGenes[order(downGenes$pvalue), ]
-# write.table(downGenes, file = "diffExpr_downregulated.txt", sep = "\t", quote = FALSE, row.names = FALSE)
-
-diffPq <- intersect(diffGenes,  pQEntrezIDs)
-
-pq_pValues <- avgDiffExpr[pQEntrezIDs, ]
-pq_pValues <- pq_pValues[order(pq_pValues$corrected), ]
-pq_pValues$corrected <- sapply(pq_pValues$corrected, function(x){format(x, digits =2, scientific = T)})
-pq_order <- rownames(pq_pValues)
-pq_plotIdx <- which(pq_order %in% c(rownames(upGenes), rownames(downGenes)))
-rownames(pq_pValues) <- sapply(rownames(pq_pValues), entrezId2Name)
-
-#Boxplots of pQ genes
-pdf(file = "diffExpr_HDregion.pdf", 9)
-par(oma = c(2,2,1,1))
-
-data <- as.data.frame(t(expr_all[pq_order, ]))
-colnames(data) <- sapply(pq_order, entrezId2Name)
-data <- cbind(inHDregion_all, data)
-v <- melt(data, id.vars="inHDregion_all")
-total <- length(pq_order)
-a <- boxplot(value~inHDregion_all+variable, v, col = c("turquoise", "orange"), ylab = "Expression", xlab = "Gene", xaxt = 'n',
-             at = c(1:(total*3))[-seq(3, total*3, 3)], ylim= c(0,12), 
-             main = "Differential expression in all brains with combined p-values")
-axis(1, at = seq(1.5, total*3,3), labels  = make.italic(colnames(data)[-1]), cex.axis = 0.8)
-legend("topright", c("Outside","Inside"), fill = c("turquoise", "orange"))
-positionLabels <- sapply(pq_plotIdx, function(x){idx <- c(x*2-1, x*2); max(a$out[which(a$group %in% idx)])+1})
-text(pq_plotIdx*3-1.5, positionLabels, labels = paste("p = ", pq_pValues$corrected[pq_plotIdx], sep = ""), cex = 0.8)
-
-lapply(donorNames, function(b){
-  expr <- brainExpr[[b]]
-  dat <- as.data.frame(t(expr[pq_order, ]))
-  colnames(dat) <- sapply(pq_order, entrezId2Name)
-  inHD <- inHDregion[[b]]
-  dat <- cbind(inHD, dat)
-  v <- melt(dat, id.vars="inHD")
-  total <- length(pq_order)
-  a <- boxplot(value~inHD+variable, v, col = c("turquoise", "orange"), ylab = "Expression", xlab = "Gene", xaxt = 'n',
-               at = c(1:(total*3))[-seq(3, total*3, 3)], ylim= c(0,12),
-               main = paste("Differential expression in ", b))
-  axis(1, at = seq(1.5, total*3,3), labels  = make.italic(colnames(dat)[-1]), cex.axis = 0.8)
-  legend("topright", c("Outside","Inside"), fill = c("turquoise", "orange"))
 })
-dev.off()
-# 
-# #Functional enrichment of up- and down-regulated genes
-# david<-DAVIDWebService$new(email="D.L.Keo@tudelft.nl", 
-#                            url="https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
-# setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL"))
-# bg_list <- probeInfo$entrez_id
-# bg <- addList(david, bg_list, idType = "ENTREZ_GENE_ID", listName = "AHBA background", listType = "Background")
-# bg
-# t <- 0.05 # EASE p-value threshold
-# setTimeOut(david, 200000)
-# 
-# result <- addList(david, upGenes$entrez_id, idType = "ENTREZ_GENE_ID", listName = "diffExpr_upregulated", listType = "Gene")
-# print(result)
-# setCurrentBackgroundPosition(david, 1)
-# getFunctionalAnnotationChartFile(david, "diffExpr_upregulated_goterms.txt", threshold=t, count=2L)
-# 
-# result <- addList(david, downGenes$entrez_id, idType = "ENTREZ_GENE_ID", listName = "diffExpr_downregulated", listType = "Gene")
-# print(result)
-# setCurrentBackgroundPosition(david, 1)
-# getFunctionalAnnotationChartFile(david, "diffExpr_downregulated_goterms.txt", threshold=t, count=2L)
+names(values) <- entrezId2Name(pQEntrezIDs)
+values
+
+#Functional enrichment of up- and down-regulated genes
+david<-DAVIDWebService$new(email="D.L.Keo@tudelft.nl",
+                           url="https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
+setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL"))
+bg_list <- probeInfo$entrez_id
+bg <- addList(david, bg_list, idType = "ENTREZ_GENE_ID", listName = "AHBA background", listType = "Background")
+bg
+t <- 0.05 # EASE p-value threshold
+setTimeOut(david, 200000)
+
+result <- addList(david, upGenes, idType = "ENTREZ_GENE_ID", listName = "diffExpr_upregulated", listType = "Gene")
+print(result)
+setCurrentBackgroundPosition(david, 1)
+getFunctionalAnnotationChartFile(david, "diffExpr_upregulated_goterms.txt", threshold=t, count=2L)
+getClusterReportFile(david, "diffExpr_upregulated_termclusters.txt", type = c("Term"))
+
+result <- addList(david, downGenes, idType = "ENTREZ_GENE_ID", listName = "diffExpr_downregulated", listType = "Gene")
+print(result)
+setCurrentBackgroundPosition(david, 1)
+getFunctionalAnnotationChartFile(david, "diffExpr_downregulated_goterms.txt", threshold=t, count=2L)
+getClusterReportFile(david, "diffExpr_downregulated_termclusters.txt", type = c("Term"))
+
+#Benjamini-corrected GO terms
+#Function to read Rdavid output
+read.RdavidOutput <- function(fileName){
+  if (file.exists(fileName)){
+    terms <- read.csv(fileName, header = TRUE, sep = "\t", colClasses = "character")
+    if (nrow(terms) == 0){
+      print("...Removed")
+      file.remove(fileName)
+      NULL
+    } else {
+      terms
+    }
+  } else {
+    NULL
+  }
+}
+
+# Read annotation chart files
+upTerms <- read.RdavidOutput("diffExpr_upregulated_goterms.txt")
+downTerms <- read.RdavidOutput("diffExpr_downregulated_goterms.txt")
+upRows <- which(upTerms$Benjamini < 0.05)
+downRows <- which(downTerms$Benjamini < 0.05)
+
+downTerms <- downTerms[downRows, c("Category", "Term", "Count", "Benjamini")]
+downTerms <- downTerms[order(downTerms$Benjamini), ] # sort by Benjamini P-value
+write.table(downTerms, file = "diffExpr_downregulated_corrected_goterms.txt", sep = "\t", row.names = FALSE)

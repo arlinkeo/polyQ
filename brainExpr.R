a1eb3bd1f690dba432f730a748e7f7b588e6d302
arlinkeo <arlinkeo@gmail.com>
2017-08-17 16:53:38 +0200

    rm

--- brainExpr.R
+++ brainExpr.R
@@ -2,7 +2,6 @@
 source("PolyQ_scripts/baseScript.R")
 
 #Read expression data, assign row- and columnnames
-# brainNames <- list("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
 brainExpr <- lapply(donorNames, function (x) {
   expr <- read.csv(paste("../ABA_human_processed/gene_expr_normalized_microarray_", x, "_2014-11-11.csv", sep = ""), header = FALSE)
   rownames(expr) <- probeInfo$entrez_id

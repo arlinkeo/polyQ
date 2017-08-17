b5402a51a8451ba98b0bda0fa97e4bea7c677f61
arlinkeo <arlinkeo@gmail.com>
2017-08-17 16:49:48 +0200

    comment

--- baseScript.R
+++ baseScript.R
@@ -1,8 +1,18 @@
 # Functions
 
 setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression")
+options(stringsAsFactors = FALSE)
+
+###INFO###
 
 probeInfo <- read.csv("../ABA_human_processed/probe_info_2014-11-11.csv")
+
+load("resources/polyQ.RData")
+
+donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
+
+
+###FUNCTIONS###
 
 entrezId2Name <- function (x) { #Input is vector or single element
   probeInfo$gene_symbol[match(x, probeInfo$entrez_id)]
@@ -17,7 +27,3 @@ make.italic <- function(x) { #For genes
   as.expression(lapply(x, function(x) bquote(italic(.(x)))))
 }
 
-
-load("resources/polyQ.RData")
-
-donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")

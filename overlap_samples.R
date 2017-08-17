f27f1669b4ec3588640f9df21dc28dd5bc26db49
arlinkeo <arlinkeo@gmail.com>
2017-08-17 14:42:56 +0200

    ggplot

--- overlap_samples.R
+++ overlap_samples.R
@@ -1,26 +1,65 @@
 # Number samples of atomic structures that fall within the HD associated regions (Coppen2016)
 setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression")
 options(stringsAsFactors = FALSE)
+library(ggplot2)
 
 # load("resources/polyQ.RData")
-# structureIDs <- structureIDs[!structureIDs$name %in% c("brain", "cerebellum"), ]
+# structureIDs <- structureIDs[!structureIDs$name %in% c("cerebellum"), ]
 #structureIDs <- rbind(HD_region = c(NA, "HDregion", "HD_region"), structureIDs)
 load("resources/brainExpr.RData") 
 donorNames <- names(brainExpr)
 names(donorNames) <- donorNames
 load("resources/sampleIDs.RData")
-sampleIDs <- sampleIDs[!names(sampleIDs) %in% c("cerebellum")]
 sampleIDs_HD <- sampleIDs[["HD_region"]]
+sampleIDs <- sampleIDs[!names(sampleIDs) %in% c("HD_region", "brain", "cerebellum")]
+
 
 # Number of samples in atomic structures and HD regions
-overlap <- lapply(sampleIDs[-1], function(s){
+overlap <- lapply(sampleIDs, function(s){
   lapply(donorNames, function(d){
     samples <- bitwAnd(s[[d]], sampleIDs_HD[[d]])
   })
 })
-sapply(overlap, function(d){sapply(d, sum)})
+tab <- sapply(overlap, function(d){sapply(d, sum)})
+totalN <- apply(tab, 2, sum)
 # 
-# # Number of samples in Hd region minus structure, should add up to toal number samples in structures
+# Total samples in and out HD region
+totalHD <- sum(sapply(sampleIDs_HD, sum))
+idHD <- unlist(sampleIDs_HD)
+notHD <- as.numeric(!idHD)
+
+allStruct <- lapply(sampleIDs, unlist)
+allStruct <-Reduce(bitwOr, allStruct)
+nonStruct <- as.numeric(!allStruct)
+restRest <- bitwAnd(nonStruct, notHD)
+
+colHD <- lapply(names(sampleIDs), function(n){
+  s <- sampleIDs[[n]]
+  inHD <- sum(bitwAnd(unlist(s), idHD))
+  nonHD <- sum(bitwAnd(unlist(s), as.numeric(!idHD)))
+  inHD <- c("HD-associated region", n, inHD)
+  nonHD <- c("rest", n, nonHD)
+  rbind(inHD, nonHD)
+})
+tab <- Reduce(rbind, colHD)
+rownames(tab) <- NULL
+tab <- gsub("_", " ", tab)
+tab <- as.data.frame(tab)
+colnames(tab) <- c("HD", "structure", "samples")
+tab[nrow(tab)+1, ] <- c("rest", "rest", sum(restRest))
+tab$samples <- as.numeric(tab$samples)
+
+recur <- function(x){
+  if (x == 1 ) tab$samples[x]
+  else tab$samples[x]+recur(x-1)
+}
+tab$ymax <- sapply(c(1:nrow(tab)), recur)
+tab$ymin <- tab$ymax-tab$samples
+
+
+ggplot(tab) + geom_rect(aes(fill = HD), ymax = ymax, ymin = ymin, xmax = 4, xmin = 3)
++ geom_rect(aes(fill = structure), ymax = ymax, ymin = ymin, xmax = 3, xmin = )
+# # Number of samples in Hd region minus structure, should add up to total number samples in structures
 # diffHD <- lapply(sampleIDs[-1], function(s){
 #   lapply(donorNames, function(d){
 #     overlapping <- bitwAnd(s[[d]], sampleIDs_HD[[d]])

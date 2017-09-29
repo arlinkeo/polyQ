aedcdf8ead27882409c7a91e372d17b4406db1fa
arlinkeo <arlinkeo@gmail.com>
2017-09-29 22:59:05 +0200

    clean

--- polyQ_region.R
+++ polyQ_region.R
@@ -2,7 +2,7 @@ setwd("C:/Users/dkeo/surfdrive/polyQ_coexpression")
 options(stringsAsFactors = FALSE)
 
 # Sample structures
-ontology <- read.csv("ABA_human_processed/Ontology_edited.csv")
+ontology <- read.csv("../ABA_human_processed/Ontology_edited.csv")
 structures <- c("brain","frontal lobe", "mesencephalon", "striatum", "hypothalamus", "pons", "parietal lobe", "cerebellar cortex", "cerebellum")
 structureIDs <- ontology[ontology$name %in% structures, ][ , c(1:3)]
 structureIDs[, 3] <- sapply(structureIDs[, 3], function(id){gsub(" ", "_", id)})
@@ -10,10 +10,10 @@ rownames(structureIDs) <- structureIDs$name
 
 #Select PolyQ genes from data
 polyQgenes <- c("HTT", "ATN1", "AR", "ATXN1", "ATXN2", "ATXN3", "CACNA1A", "ATXN7", "TBP")
-probeInfo <- read.csv("ABA_human_processed/probe_info_2014-11-11.csv")
-pQgeneInfo <- probeInfo[probeInfo$gene_symbol %in% polyQgenes, ]
-pQEntrezIDs <- as.character(pQgeneInfo[ , 6])
-polyQgenes <- pQgeneInfo[ , 4]
+probeInfo <- read.csv("../ABA_human_processed/probe_info_2014-11-11.csv")
+pqrows <- which(probeInfo$gene_symbol %in% polyQgenes)
+pQEntrezIDs <- as.character(probeInfo$entrez_id[pqrows])
+polyQgenes <- probeInfo$gene_symbol[pqrows]
 
 #Gephi node colors
 library(RColorBrewer)

09cfde4f91e5a225d39d4ca40b04634d760210e3
arlinkeo <arlinkeo@gmail.com>
2017-10-20 16:33:59 +0200

    comments

--- pieChart_samples.R
+++ pieChart_samples.R
@@ -7,30 +7,35 @@ load("resources/sampleIDs.RData")
 sampleIDs["cerebellum"] <- NULL
 ontology <- read.csv("../ABA_human_processed/Ontology_edited.csv")
 
+# Count number of samples within each brain region and donor
 nSamples <- sapply(sampleIDs, function(r){
   sapply(r, sum)
 })
 
+#Get or assign colors of brain regions
 structures <- names(sampleIDs)[-c(which(names(sampleIDs) == "HD_region"))]
 structures <- sapply(structures, function(s)gsub("_", " ", s))
-colorOther <- "grey"
+colorOther <- "grey" # color for samples not assigned to one of the regions
 colorsStruct <- sapply(ontology$color_hex_triplet[match(structures, ontology$name)], function(c){paste0("#", c)})
 colorsStruct <- c(colorsStruct[-1], colorOther, colorsStruct[1])
 colorsHD <- c("#956b53", colorOther)
 
-sumDonors <- apply(nSamples, 2, sum)
+# Pie chart info of samples in HD-associated region
+sumDonors <- apply(nSamples, 2, sum) # sum across donors
 nonHD <- sumDonors["brain"]-sumDonors["HD_region"]
 vecHD <- c(sumDonors["HD_region"], nonHD)
 names(vecHD) <- c("HD-associated region", "rest")
 names(vecHD) <- paste0(names(vecHD), " (", vecHD, ")")
 sum(vecHD)
 
+# Pie chart info of samples in anatomical regions
 sumStruct <- sumDonors[-which(names(sumDonors) %in% c("brain", "HD_region"))]
 nonStruct <- sumDonors["brain"] - sum(sumStruct)
 vecStruct <- c(sumStruct, nonStruct)
 names(vecStruct) <- c(structures[-1], "rest")
 names(vecStruct) <- paste0(names(vecStruct), " (", vecStruct, ")")
 
+#Print both pie charts in pdf-file
 pdf(file = "Images/pieChart_samples.pdf", 12, 5)
 layout(matrix(c(1:2), 1, 2))
 par(mai = c(0,1,0,1), oma = c(0,2,0,2))
